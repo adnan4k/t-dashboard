ef7959b533eec30d76103443424b489d34ca8bb9
adnan4k <fayomuhe5@gmail.com>
2024-11-25 21:08:26 +0300

    some clean up on login page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,12 +3,9 @@
     @auth()
         {{-- If the user is authenticated on the static sign up or the sign up page --}}
         @if (in_array(request()->route()->getName(),['static-sign-up', 'sign-up'],))
-            @include('layouts.navbars.guest.sign-up')
             {{ $slot }}
-            @include('layouts.footers.guest.with-socials')
             {{-- If the user is authenticated on the static sign in or the login page --}}
         @elseif (in_array(request()->route()->getName(),['sign-in', 'login'],))
-            @include('layouts.navbars.guest.login')
             {{ $slot }}
             @include('layouts.footers.guest.description')
         @elseif (in_array(request()->route()->getName(),['profile', 'my-profile'],))
@@ -40,19 +37,11 @@
     @guest
         {{-- If the user is on the login page --}}
         @if (!auth()->check() && in_array(request()->route()->getName(),['login'],))
-            @include('layouts.navbars.guest.login')
             {{ $slot }}
-            <div class="mt-5">
-                @include('layouts.footers.guest.with-socials')
-            </div>
+          
 
             {{-- If the user is on the sign up page --}}
         @elseif (!auth()->check() && in_array(request()->route()->getName(),['sign-up'],))
-            <div>
-                @include('layouts.navbars.guest.sign-up')
-                {{ $slot }}
-                @include('layouts.footers.guest.with-socials')
-            </div>
         @endif
     @endguest
 
--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -1,15 +1,1 @@
-<footer class="footer pb-4">
-    <div class="container-fluid">
-        <div class="row align-items-center justify-content-lg-between">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-            <div class="col-lg-6 mb-lg-0 mb-4">
-                <div class="copyright text-center max-w-48 text-sm text-muted text-lg-left">
-                    © {{ now()->year }} Developed by <a style="color: #252f40;" href="https://www.creative-tim.com" class="max-w-48 font-weight-bold ml-1"
-                        target="_blank">Kasma Tech Solutions</a>  
-                </div>
-            </div>
-            </div>
-         
-        </div>
-    </div>
-</footer>
+-s
