078ebfe24a6c8181e94c8c9741a88417edbd91a0
adnan4k <fayomuhe5@gmail.com>
2024-11-26 22:06:50 +0300

    toast added and some unnecessary file removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import '../../vendor/masmerise/livewire-toaster/resources/js'; 
 import PerfectScrollbar from 'perfect-scrollbar';
 window.PerfectScrollbar = PerfectScrollbar;
-import EasyMDE from "easymde";
 
 require('./bootstrap');
 require('./custom');
